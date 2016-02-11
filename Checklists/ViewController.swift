3cad9163d1a04e63f3309556211836dab2d7dc7f
YasB <costanza.yasmina@gmail.com>
2016-02-11 12:04:57 +0100

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Visibrain on 11/02/2016.
+//  Copyright © 2016 Costanza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
